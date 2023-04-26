df85abb013d7def4bc9900ebf5f4fa7d4955ee97
Nemanja Risteski <risteski.nemanja5@gmail.com>
2023-04-26 17:13:18 -0400

    Refactoring- fetch games with react query

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -19,7 +19,7 @@ const PlatformSelector = ({ onSelectPltaform, selectedPlatform }: Props) => {
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
+        {data?.results.map((platform) => (
           <MenuItem
             onClick={() => onSelectPltaform(platform)}
             key={platform.id}
