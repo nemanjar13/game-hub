879046569d06b9f3f6be1fcb685426f6c3e11d4a
Nemanja Risteski <risteski.nemanja5@gmail.com>
2023-04-26 22:52:48 -0400

    Implement infinite scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -31,7 +31,8 @@ useInfiniteQuery<FetchResponse<Game>, Error>({
     }),
   getNextPageParam: (lastPage, allPages) => {
     return lastPage.next ? allPages.length + 1 : undefined;
-  }
+  },
+  staleTime: 24 * 60 * 60 * 1000 //24h
 })
 
 export default useGames;
